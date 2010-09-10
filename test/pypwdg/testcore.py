725f3042ae9f7e5dd037c5e2c2ca566444889964
Joel Phillips <joel.phillips@reading.ac.uk>
2010-09-10 10:13:21 +0100

    Working adaptivity? ...

--- test/pypwdg/testcore.py
+++ test/pypwdg/testcore.py
@@ -5,7 +5,9 @@ Created on Aug 5, 2010
 '''
 import unittest
 
-import pypwdg.core
+import pypwdg.core.bases as pcb
+import pypwdg.core.adaptivity as pca
+import pypwdg.utils.quadrature as puq
 import numpy
 from test.pypwdg import __path__ as path
 
@@ -85,6 +87,18 @@ class TestVandermondes(unittest.TestCase):
             self.assertEqual(d.shape, (numquads, 1))
             self.assertEqual(LV.numbases[faceid], 1)
         
+class TestAdaptivity(unittest.TestCase):
+    def testOptimalBasis(self):
+        """ Can we find the right direction to approximate a plane wave?"""
+        k = 10
+        g = pcb.PlaneWaves(numpy.array([[3.0/5,4.0/5]]), k).values
+#        g = pcb.FourierBessel(numpy.array([-2,-1]), numpy.array([5]),k)
+        npw = 3
+        nq = 8
+        gen, ini = pca.pwbasisgeneration(k, npw)
+        triquad = puq.trianglequadrature(nq)
+        basis, coeffs, l2err = pca.optimalbasis(g, gen, ini, triquad, True)
+        self.assertAlmostEqual(l2err,0)
         
 
 if __name__ == "__main__":
