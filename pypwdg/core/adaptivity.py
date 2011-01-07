bc116775d05136fc080c8e2c323965396121a498
Joel Phillips <joel.phillips@reading.ac.uk>
2011-01-07 10:42:20 +0000

    ...

--- pypwdg/core/adaptivity.py
+++ pypwdg/core/adaptivity.py
@@ -14,51 +14,58 @@ def augmentparams(params, npw, dim):
     if len(params) < npw:
         params = np.vstack((params, pcb.circleDirections(len(params) - npw)))
     return params
-        
 
-class AdaptivePWBasisCreator(object):
-    def __init__(self, k, origin, npw, nfb, params = None):
+def normalise(params, npw):
+    p = params.reshape(npw, -1)
+    return p / np.sqrt(np.sum(p**2, axis=1)).reshape(npw,1)        
+
+class PWFBCreator(object):
+    def __init__(self, k, origin, npw, nfb, params = None):        
         self.k = k
         self.origin = origin
         self.npw = npw
         self.nfb = nfb
+        self.n = npw + nfb        
         self.params = augmentparams(params, npw, len(origin))
-        self.n = npw + nfb
     
-    def normalise(self, params):
-        p = params.reshape(self.npw, -1)
-        return p / np.sqrt(np.sum(p**2, axis=1)).reshape(self.npw,1)        
-        
-    def testbasis(self, params):
-        return pcb.PlaneWaves(self.normalise(params), self.k)
-        
-    def fullbasis(self, params):
-        pwbasis = self.testbasis(params)
+    def pwbasis(self, params):
+        return pcb.PlaneWaves(normalise(params, self.npw), self.k)
+    
+    def newparams(self, params):
+        return PWFBCreator(self.k,self.origin,self.npw,self.nfb,params)
+     
+    def getbasis(self):
+        pwbasis = self.pwbasis(self.params)
         fbbasis = pcb.FourierBessel(self.origin, np.arange(0,self.nfb) - self.nfb/2)
         return pcb.BasisCombine([pwbasis, fbbasis])
-     
-    def finalise(self, params):
-        return AdaptivePWBasisCreator(self.k, self.origin, self.npw, self.nfb, self.normalise(params))
-    
+
     def nearbybases(self, x):
         nearby = [self]
-        nearby.append(AdaptivePWBasisCreator(self.k, self.origin, self.npw, self.nfb+1, self.params))
-        if self.nfb > 3: nearby.append(AdaptivePWBasisCreator(self.k, self.origin, self.npw, self.nfb-1, self.params))
-        nearby.append(AdaptivePWBasisCreator(self.k, self.origin, self.npw+1, self.nfb, self.params))
-        if self.npw > 0: nearby.append(AdaptivePWBasisCreator(self.k, self.origin, self.npw-1, self.nfb, self.params[np.abs(x) != np.min(np.abs(x))]))
+        k,origin,npw,nfb, params = self.k,self.origin,self.npw,self.nfb, self.params
+        nearby.append(PWFBCreator(k,origin,npw,nfb+1,params))
+        if nfb > 3: nearby.append(PWFBCreator(k,origin,npw,nfb-1,params))
+        nearby.append(PWFBCreator(k,origin,npw+1,nfb,params))
+        if npw > 0: nearby.append(PWFBCreator(k,origin,npw,nfb,params[np.abs(x) != np.min(np.abs(x))]))
         return nearby
 
 def origin(mesh, e):
     return np.average(mesh.nodes[mesh.elements[e]], axis = 0)
 
-        
-@distribute
-class AdaptivePWBasisManager(object):
+class AdaptiveElementToBasis(object):
     
-    def __init__(self, mesh, k, npw, nfb):
-        etopwbc = dict([(e, AdaptivePWBasisCreator(k, origin(mesh, e), npw, nfb, None)) for e in mesh.partition])
+    def __init__(self, mesh, k, npw, nfb, etopwfbc = None):
+        if etopwfbc is None: 
+            self.etopwfbc = dict([(e, PWFBCreator(k, origin(mesh, e), npw, nfb)) for e in range(mesh.nelements)])
+        else: self.etopwfbc = etopwfbc
     
-    def buildEtoB(self):
+    def getValues(self, eid, points, normal=None):
+        """ Return the values of the basis for element eid at points"""
+        return self.etopwfbbc[eid].getbasis().values(points, normal)
+    
+    def getDerivs(self, eid, points, normal):
+        return self.etopwfbbc[eid].getbasis().derivs(points, normal)
+
+    def 
     
     
 origin=np.average(self.mesh.nodes[self.mesh.elements[e]], axis = 0)
