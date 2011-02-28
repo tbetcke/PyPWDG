6260f81e10560f91ca4da329da78a24239e6ace0
Richard Dodd <richard.o.dodd@gmail.com>
2011-02-28 11:05:49 +0000

    Added first domain decomposition file/testing push to branch

---
+++ experiments/domaindecomp/firstdd.py
@@ -0,0 +1,28 @@
+import pypwdg.setup as ps
+import pypwdg.core.bases as pcb
+import pypwdg.mesh.mesh as pmm
+import pypwdg.core.boundary_data as pcbd
+
+from numpy import array,sqrt
+
+k = 60
+direction=array([[1.0,1.0]])/sqrt(2)
+g = pcb.PlaneWaves(direction, k)
+impbd = pcbd.generic_boundary_data([-1j*k,1],[-1j*k,1],g)
+
+#bnddata={7:impbd, 
+#         8:impbd}
+bnddata={7:pcbd.dirichlet(g), 
+         8:pcbd.dirichlet(g)}
+
+bounds=array([[0,1],[0,1]],dtype='d')
+npoints=array([100,100])
+
+mesh = pmm.gmshMesh('square.msh',dim=2)
+bases = pcb.planeWaveBases(mesh,k,nplanewaves=15)
+
+problem=ps.Problem(mesh,k,20, bnddata)
+solution = ps.Computation(problem, bases).solve()
+solution.writeSolution(bounds,npoints,fname='square.vti')
+problem.writeMesh(fname='square.vtu',scalars=solution.combinedError())
+
