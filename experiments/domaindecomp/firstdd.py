ab8b0db59d137e17b17a6669f94d8f28f46a3435
Richard Dodd <nw024833@live.reading.ac.uk>
2011-03-02 13:26:41 +0000

    push experiment for Joel

--- experiments/domaindecomp/firstdd.py
+++ experiments/domaindecomp/firstdd.py
@@ -6,30 +6,34 @@ import pypwdg.core.evaluation as pce
 from numpy import array,sqrt,vstack,ones,linspace
 import pylab as pl
 
-k = 60
+k = 10
 direction=array([[1.0,1.0]])/sqrt(2)
 g = pcb.PlaneWaves(direction, k)
 impbd = pcbd.generic_boundary_data([-1j*k,1],[-1j*k,1],g)
 
-bnddata={7:pcbd.dirichlet(g), 
+bnddata1={7:pcbd.zero_neumann(), 
          8:pcbd.dirichlet(g),
          9:pcbd.dirichlet(g),
          10:pcbd.dirichlet(g)}
 
 bounds=array([[0,1],[0,1]],dtype='d')
-npoints=array([100,100])
+npoints=array([1000,1000])
 
 mesh = pmm.gmshMesh('square.msh',dim=2)
-bases = pcb.planeWaveBases(mesh,k,nplanewaves=15)
+bases = pcb.planeWaveBases(mesh,k,nplanewaves=30)
 
-problem=ps.Problem(mesh,k,20, bnddata)
-solution = ps.Computation(problem, bases).solve()
+problem1 = ps.Problem(mesh,k,20, bnddata1)
+solution1 = ps.Computation(problem1, bases).solve()
 
-points = vstack([linspace(0, 1, 1000), ones(1000)]).transpose()
-evalu = pce.Evaluator(mesh, solution.elttobasis, array(points), direction=(1, -1))
-vals, derivs = evalu.evaluate(solution.x)
+points = vstack([ones(1000), linspace(0, 1, 1000)]).transpose()
+#print points
+evalu = pce.Evaluator(mesh, solution1.elttobasis, array(points), direction=(1, -1))
+vals, derivs = evalu.evaluate(solution1.x)
+
+solution1.writeSolution(bounds,npoints,fname='firstdd.vti')
+problem1.writeMesh(fname='firstdd.vtu',scalars=solution1.combinedError())
 
 pl.plot (vals)
-pl.plot (derivs)
+pl.plot (derivs/sqrt(2) * 1./k)
 pl.show()
 
