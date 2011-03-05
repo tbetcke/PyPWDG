74dcd3afbc9e4878697f3db15815e5c6ce6286f1
Joel Phillips <joel.phillips@reading.ac.uk>
2011-03-05 13:44:40 +0000

    fixing unittest

--- pypwdg/core/tests/testvandermondes.py
+++ pypwdg/core/tests/testvandermondes.py
@@ -25,8 +25,7 @@ class TestVandermondes(unittest.TestCase):
         meshes = tum.examplemeshes2d()
         for mesh in meshes:
             mq = pmm.MeshQuadratures(mesh, puq.legendrequadrature(numquads))
-            e2b = pcb.ElementToBases(mesh)
-            e2b.addUniformBasis(pw)
+            e2b = pcb.constructBasis(mesh, pcb.UniformBases([pw]))
             LV = pcv.LocalVandermondes(mesh, e2b, mq)
             for faceid in range(mesh.nfaces):
                 v = LV.getValues(faceid)
