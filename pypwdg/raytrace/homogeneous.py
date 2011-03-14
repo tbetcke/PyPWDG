70cc30719bf5e98cda6f1ca059c9ceb60b57e568
Joel Phillips <joel.phillips@reading.ac.uk>
2011-03-14 22:09:06 +0000

    ...

--- pypwdg/raytrace/homogeneous.py
+++ pypwdg/raytrace/homogeneous.py
@@ -10,5 +10,24 @@ def trace(point, direction, mesh, reflectingbdys, nref=5):
     reflectingbdys: entities that should reflect the ray.
     nref: number of reflections
     '''
+    pass
     
+class HomogenousTrace(object):
+    ''' Knows how to trace through one element assuming homogenous material data''' 
     
+    def __init__(self, mesh, reflectingbdys):
+        self.mesh = mesh
+        self.rbs = reflectingbdys
+                
+    def trace(self, face, direction):
+        ''' Given an input face and a direction trace a path through the relevant element
+        
+        Returns: (e, f, d) where:
+            e is the element that was traced through, 
+            f is the next input face
+            d is the next direction'''
+        
+        e = self.mesh.ftoe[face]
+        fs = self.mesh.etof[e]
+        
+         
