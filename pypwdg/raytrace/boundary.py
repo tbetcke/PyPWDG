8b61ded54a39583d653324c6d6a4aeebf20e8181
Joel Phillips <joel.phillips@reading.ac.uk>
2011-03-23 19:12:47 +0000

    ...

--- pypwdg/raytrace/boundary.py
+++ pypwdg/raytrace/boundary.py
@@ -4,11 +4,31 @@ Created on Mar 22, 2011
 @author: joel
 '''
 
-def initialrt(mesh, bdydata, direction, maxspace):
+import pypwdg.utils.optimisation as puo
+
+class BoundaryDataFit(object):
+    def __init__(self, bc, normal, quadrule):
+        self.qp, qw = quadrule
+        self.qwsqrt = np.sqrt(qw).ravel()
+        rc = bc.r_coeffs
+        self.urp = (bc.values(self.qp) * rc[0] + bc.derivs(self.qp) * rc[1]).ravel() * self.qwsqrt
+        self.l2urp = math.sqrt(np.vdot(self.urp,self.urp))
+    
+    def optimise(self, basis):
+        basisvals = basis.values(self.qp) * self.qwsqrt.reshape(-1,1)
+        coeffs = sl.lstsq(basisvals, self.urp)[0]
+        err = np.abs(np.dot(basisvals, coeffs).ravel() - self.urp)/self.l2urp
+        return coeffs, err
+
+def initialrt(mesh, bdydata, k, mqs, maxspace):
     """ Find starting points and directions for ray-tracing """
     
     for (bdy, bc) in bdydata.items():
         faces = mesh.entityfaces[bdy]            
         for f in faces.tocsr().indices:
-            
+            gen, ini = puo.pwbasisgeneration(k, 1)
+            x = mqs.quadpoints(f)
+            w = mqs.quadweights(f)
+            linearopt = puo.LeastSquaresFit()
+
             
