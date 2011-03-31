303e18e946eb75a5351ab953c8b4a914b30b9bd8
Joel Phillips <joel.phillips@reading.ac.uk>
2011-04-01 00:21:05 +0100

    more Dubiner polynomials ...

--- pypwdg/utils/polynomial.py
+++ pypwdg/utils/polynomial.py
@@ -4,13 +4,24 @@ Created on Mar 31, 2011
 @author: joel
 '''
 import numpy as np
+import scipy.special.orthogonal as sso 
+
+def jacobidnorm(N,a,b,d,x):
+    """ Return the dth derivative of the [d,N] shifted Jacobi(a,b) polynomials at x
+    
+        The polynomials are normalised to have weighted L^2 norm 1. (although, obviously
+        not the derivatives)
+    """
+    n = np.arange(N+1, dtype=float)
+    norms = np.sqrt((1 /(2*n+a+b+1)) * (sso.poch(n + 1, a) / sso.poch(n + b + 1, a)))
+    return jacobid(N,a,b,d,x) / norms
 
 def jacobid(N,a,b,d,x):
-    """ Return the dth derivative of the [d,N] shifted Jacobi(a,b) polynomials at x"""
-    from scipy.special.orthogonal import poch
+    """ Return the dth derivative of the [d,N] shifted Jacobi(a,b) polynomials at x  """
     v = getJacobi(a+d,b+d)(N-d, x)   
-    fv = poch(np.arange(a+b+d+1, a+b+N+2), d)[:,np.newaxis] * v
-    return np.vstack((np.zeros((d, len(x))), fv )).T
+    fv = sso.poch(np.arange(a+b+d+1, a+b+N+2), d)[:,np.newaxis] * v
+
+    return np.vstack((np.zeros((d, len(x))), fv )).T 
 
 jacobicache = {}
 def getJacobi(a,b):
@@ -36,7 +47,7 @@ class Jacobi(object):
         self.A[2:] = 2*n*(n+a+b)/((2*n+a+b)*(2*n+a+b-1))
         self.B[2:] =-(a*a-b*b)/((2*n+a+b)*(2*n+a+b-2))
         self.C[2:] =2*(n+a-1)*(n+b-1)/((2*n+a+b-1)*(2*n+a+b-2))
-
+    
     def __call__(self,N,x):
         v = [np.zeros(len(x))]
         x = 2*x - 1
@@ -52,17 +63,28 @@ class DubinerTriangle(object):
         As things stand, things will probably fail at (0,1).    
     """
     def __init__(self,k, p):
-        self.eta2 = p[:,1]
-        self.eta1 = p[:,0] / (1-self.eta2)  
+        x = p[:,0]
+        y = p[:,1]
+        self.eta2 = y
+        self.eta1 = x / (1-y)  
         self.k = k
-        self.P1 = jacobid(k,0,0,0,self.eta1)
-        self.P2 = [jacobid(k - i, 2*i +1, 0, 0, self.eta2) for i in range(k+1)] 
-        
+        self.P1 = jacobidnorm(k,0,0,0,self.eta1)
+        self.P2 = [jacobidnorm(k - i, 2*i +1, 0, 0, self.eta2) for i in range(k+1)]
+        n = np.arange(k+1)[np.newaxis,:]
+        w = (1-self.eta2[:,np.newaxis])**n
+        self.P1w = self.P1 * w 
+        wD = np.hstack((np.zeros((len(p),1)), w[:,[-1]] * n[:,1:])) 
+        self.P1wD = jacobidnorm(self.k,0,0,1,self.eta1) * w + self.P1w *wD 
+        self.P2D = [jacobidnorm(self.k - i, 2*i +1, 0, 1, self.eta2) for i in range(self.k+1)]
+        self.J = np.array([[1/(1-y), np.zeros(len(p))],[x/(1-y)**2, np.ones(len(p))]])
         
     def values(self):
-        return np.hstack([(self.P1[:,i] * (1-self.eta2)**i).reshape(-1,1) * self.P2[i] for i in range(self.k+1)])
+        return np.hstack([self.P1w[:,[i]]  * self.P2[i] for i in range(self.k+1)])
     
     def derivs(self):
-        self.P1D = jacobid(self.k,0,0,1,self.eta1)
-        self.P2D = [jacobid(self.k - i, 2*i +1, 0, 1, self.eta2) for i in range(self.k+1)]
-        pass
+        Deta1 = np.hstack([self.P1wD[:,[i]]  * self.P2[i] for i in range(self.k+1)])
+        Deta2 = np.hstack([self.P1w[:,[i]]  * self.P2D[i] for i in range(self.k+1)])
+        etagrad = np.array([Deta1, Deta2])
+        return np.sum(self.J[...,np.newaxis] * etagrad[np.newaxis,...], axis=1)
+        
+        
--- pypwdg/utils/tests/testpolynomial.py
+++ pypwdg/utils/tests/testpolynomial.py
@@ -20,11 +20,12 @@ class TestJacobi(unittest.TestCase):
         for a in range(MaxAB):
             for b in range(MaxAB):
                 # first lets test some orthogonality:
-                P = pup.jacobid(N-1, a, b, 0, x) # get the first N jacobi polynomials evaluated at x                
+                P = pup.jacobidnorm(N-1, a, b, 0, x) # get the first N jacobi polynomials evaluated at x                
                 jw = w * x**b * (1-x)**a # combine quadrature weight with orthogonality weight
                 PP = np.dot(P.T * jw, P)
-                norm2 = (1 /(2*n+a+b+1)) * (sso.poch(n + 1, a) / sso.poch(n + b + 1, a))
-                np.testing.assert_almost_equal(PP, np.diag(norm2))
+#                norm2 = (1 /(2*n+a+b+1)) * (sso.poch(n + 1, a) / sso.poch(n + b + 1, a))
+#                np.testing.assert_almost_equal(PP, np.diag(norm2))
+                np.testing.assert_almost_equal(PP, np.eye(N,N))
     
     def testDerivative(self):
         N = 4
@@ -43,12 +44,31 @@ class TestDubiner(unittest.TestCase):
     
     def testValues(self):
         N = 5        
-        for k in range(2,N):
+        for k in range(N):
             x,w = puq.trianglequadrature(k+1)
             dt = pup.DubinerTriangle(k, x)
             V = dt.values()
             n = ((k+1)*(k+2))/2
-            self.assertEquals(V.shape[1], n)
+            self.assertEquals(V.shape[1], n) # test that we get the correct number of basis functions
             VV = np.dot(V.T, w * V)
-            print VV
-            np.testing.assert_almost_equal(VV, np.eye(n))
+            np.testing.assert_almost_equal(VV, np.eye(n)) # test that they are orthonormal
+            
+    def testDerivs(self):
+        N = 5
+        h = 1E-6
+        for k in range(N):
+            x,w = puq.trianglequadrature(k+1)
+            dt = pup.DubinerTriangle(k,x)
+            dtV = dt.values()
+            dtxhV = pup.DubinerTriangle(k, x+[h,0]).values()
+            dtyhV = pup.DubinerTriangle(k, x+[0,h]).values()
+            dtD = dt.derivs()
+            dtxhD = (dtxhV - dtV)/h
+            dtyhD = (dtyhV - dtV)/h
+            print dtD
+            print dtxhD
+            print dtyhD
+            
+            np.testing.assert_almost_equal(dtD[0],dtxhD)
+            np.testing.assert_almost_equal(dtD[1],dtyhD)
+                        
