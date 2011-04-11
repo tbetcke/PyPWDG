9dc181ea86c6526dc0d2668715395a15e8e2b4ef
Joel Phillips <joel.phillips@reading.ac.uk>
2011-04-11 16:40:47 +0100

    high polynomial investigations

--- examples/2D/squarecommon.py
+++ examples/2D/squarecommon.py
@@ -8,10 +8,10 @@ import pypwdg.utils.geometry as pug
 
 import numpy as np
 
-k = 30
+k = 15
 direction=np.array([[1.0,1.0]])/np.sqrt(2)
-#g = pcb.PlaneWaves(direction, k)
-g = pcb.FourierHankel([-2,-2], [0], k)
+g = pcb.PlaneWaves(direction, k)
+#g = pcb.FourierHankel([-2,-2], [0], k)
 impbd = pcbd.generic_boundary_data([-1j*k,1],[-1j*k,1],g)
 
 #bnddata={7:impbd, 
@@ -29,8 +29,9 @@ gvals[idx] = g.values(points)
 l2g = np.sqrt(np.vdot(gvals, gvals) / sp.length)
 
 mesh = pmm.gmshMesh('square.msh',dim=2)
-problem=ps.Problem(mesh,k,9, bnddata)
-#problem.setParams(alpha=1.0/100, beta = 10)
+problem=ps.Problem(mesh,k,30, bnddata)
+h = 0.1
+problem.setParams(alpha = 1/(k * h), beta = k * h)
 
 print mesh.nelements
 
