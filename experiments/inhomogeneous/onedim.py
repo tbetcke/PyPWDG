17356fcacd59f9eb85a370c6a839293dfb5de4d9
Joel Phillips <joel.m.phillips@gmail.com>
2011-07-19 16:28:26 +0100

    working helmholtz

--- experiments/inhomogeneous/onedim.py
+++ experiments/inhomogeneous/onedim.py
@@ -3,46 +3,65 @@ Created on Jul 11, 2011
 
 @author: joel
 '''
-import instant.config
-instant.config.get_status_output('env')
-print instant.config.get_status_output('echo $PATH')
+#import instant.config
+#instant.config.get_status_output('env')
+#print instant.config.get_status_output('echo $PATH')
+#
+##instant.config.check_and_set_swig_binary("swig", "/opt/local/bin")
+#print instant.config.get_swig_version()
+#print instant.config.get_swig_binary()
+#import sys
+#print sys.path
 
-#instant.config.check_and_set_swig_binary("swig", "/opt/local/bin")
-print instant.config.get_swig_version()
-print instant.config.get_swig_binary()
-import sys
-print sys.path
+import pypwdg.setup.computation as psc
+
+import scipy.sparse as ss
+import numpy as n
 
 from dolfin import *
 
-def fenics1dhelmholtz(N, p, k):
-    mesh = UnitInterval(N)
-    V = FunctionSpace(mesh, 'Lagrange', p)
-    VC = V * V
-    ur,ui = TrialFunction(VC)
-    vr,vi = TestFunction(VC)
-    gr = Expression("k*cos(k*x[0])")
-    gi = Expression("k*sin(k*x[0])")
-    n = FacetNormal(mesh)
-    
-    gr.k = k
-    gi.k = k
-    
-    a = (dot(grad(ur), grad(vr)) + dot(grad(ui), grad(vi)) \
-         - k**2 * (inner(ur,vr) + inner(ui,vi)))*dx\
-         + k * (dot(ur,vi) + dot(ui,vr)) * ds 
-    
-    L = (gi * vr * (1.0+n) + gr *vi*(1.0+n))*ds
-    
-    problem = VariationalProblem(a,L)
-    (Ur,Ui) = problem.solve().split()
-    plot(Ur)
-    plot(Ui)
-    interactive()
+def ssmatrix(A):
+    Ap, Ai, Ad = A.data()
+    return ss.csr_matrix((Ad, Ai, Ap))
 
-    
-    
-    
+#def fenics1dhelmholtz(N, p, k):
+N = 50
+p = 3
+k = 80
+mesh = UnitInterval(N)
+V = FunctionSpace(mesh, 'Lagrange', p)
+u = TrialFunction(V)
+v = TestFunction(V)
+gr = Expression("cos(k*x[0])")
+gi = Expression("sin(k*x[0])")
+n = FacetNormal(mesh)
 
-if __name__ == '__main__':
-    fenics1dhelmholtz(50,1,10)
+gr.k = k
+gi.k = k
+
+a = (dot(grad(u), grad(v)) - k**2 * (inner(u,v)))*dx
+b = k * dot(u,v) * ds 
+
+f = (gr * v * (1.0+n))*ds
+g = (gi * v * (1.0+n))*ds
+
+A = assemble(a)
+B = assemble(b)
+F = assemble(f)
+G = assemble(g)
+
+M = ssmatrix(A) + 1j * ssmatrix(B)
+L = 1j * k * (F.data() + 1j * G.data())
+X = psc.DirectSolver().bestSolve(M, L)
+print X
+x = uBLASVector(len(X))
+x[:] = X.real.copy()
+xfn = Function(V,x) 
+p = plot(xfn)
+#
+    
+#    
+#    
+#
+#if __name__ == '__main__':
+#    fenics1dhelmholtz(20,1,5)
