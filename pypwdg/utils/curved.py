90450caa3c0983a860d36762659593fa51534974
Joel Phillips <joel.m.phillips@gmail.com>
2011-09-02 00:39:31 +0100

    CurvedMapping working

--- pypwdg/utils/curved.py
+++ pypwdg/utils/curved.py
@@ -7,72 +7,75 @@ import scipy.optimize as so
 import scipy.interpolate as si
 import numpy as np
 
-def nearestpoint(point, surface):
-    ''' returns the parameters giving the nearest point on surface to point'''
-    s = so.fmin_powell(lambda s: np.sum((surface(s) - point)**2), np.zeros(len(point)-1))[0]
-    return s
+def nearestpoint(x, S):
+    ''' S is a chart.  x is a point.  returns y, where y minimises |S(y) - x|'''
+    x = x.reshape(1,-1)
+    return so.fmin_powell(lambda y: np.sum((S(y) - x)**2), np.zeros(x.shape[1]-1), disp=False)
 
 def uniformreferencepoints(dim, n):
+    ''' Gives a uniform grid of points on the reference dim-simplex with spacing 1/n'''
     a = np.linspace(0,1,n)
     p = np.array([np.tile(a.reshape(1 - 2*s), n*(1-s)+s) for s in np.eye(dim)]).reshape(dim,-1).transpose() # This produces a grid of points on the unit hypercube of dimension dim (I promise!)
     return p[p.sum(1) <=1] # We only want the points on the unit simplex
     
-    
+def appendcolumn(a, v=1):
+    ''' Append a uniform column to the matrix a (useful for barycentric coordinates)'''
+    return np.hstack((a, np.ones((len(a),1))*v))        
 
-class FaceMapping:
-    ''' Given a simplex, an identification of a subsimplex (vertex, edge or face) and a surface,
-        maps the whole simplex so that the subsimplex now lies on the surface
+class CurvedMapping:
+    ''' Given an m-simplex K, a sub-simplex F (i.e. vertex, edge, face, etc) and a (n-1)-manifold, M, find
+        a (smooth) mapping of K that maps the F onto the M, but is the identity
+        on any sub-simplices not intersecting F.
+        
+        The manifold is given by the chart S:U -> M \subset R^n, where U \subset R^{n-1}.        
     
-        surface should be a callable representing a chart mapping, s: U \subset R^{n-1} \rightarrow S \subset R^n.  
-        
-        The face, F is given by its vertices.  FaceMapping attempts to determine a map p:F \rightarrow U such that
-        for any x \in F
-        
-        f := s \circ p : F \rightarrow S minimises \abs{f(x) - x}    
-         
-        It's expensive to solve this optimisation problem for every x, so FaceMapping in fact determines p for a
-        given array of points and then uses a piecewise linear interpolation.
-        
-        To evaluate the map, points that do not lie on the face are first projected onto the face 
-        along the line that passes through othervertex.
+        vertices: determine the simplex, K (an m+1 x n array)
+        subidx: the indices of the vertices that determine the sub-simplex
+        surface: a callable giving a parameterisation of the surface, S
+        interpolationpoints: see below                
         
         TODO: 
         - Allow for other "nearest point" functions.
         - Allow for other interpolation options for p         
     '''
     
-    def __init__(self, vertices, subsimplexids, surface, interpolationpoints = 10):
+    def __init__(self, vertices, subidx, surface, interpolationpoints = 10):
         
         self.dim = len(vertices)-1
-        ssdim = len(subsimplexids)-1
-        self.barycentric = np.linalg.inv(np.hstack((vertices, np.ones((len(vertices),1)))))
+        subdim = len(subidx)-1
+        self.barycentric = np.linalg.pinv(appendcolumn(vertices)) # matrix that transforms a point into barycentric coordinates 
+                                                                  # using .pinv allows us to work with simplices that are not n-dimensional
         
-        # The documentation lies a bit.  In fact, we're going to determine pp : FF \rightarrow U, where
-        # FF is the reference simplex and so f = s \circ pp \circ \phi^{-1}, where \phi: FF \rightarrow F
-        # is a chart mapping for the flat face, F 
-        if ssdim >0:
-            refpoints = uniformreferencepoints(ssdim, interpolationpoints) # Get a grid of points on the reference (sub)simplex 
-            barycentricrefpoints = np.hstack((refpoints, 1-np.sum(refpoints, axis=1).reshape(-1,1)))
-            sspoints = np.dot(barycentricrefpoints, vertices[subsimplexids, :])
+        if subdim >0:
+            refpoints = uniformreferencepoints(subdim, interpolationpoints) # Get a grid of points on the reference sub-simplex
+                                                                            # Note that these are the first m-1 barycentric coordinates 
+            subpoints = np.dot(np.hstack((refpoints, 1-np.sum(refpoints, axis=1).reshape(-1,1))), vertices[subidx, :]) # And map them into meat-space
             
-            sssurfparams = np.array(map(nearestpoint, sspoints)) # For each point, x \in F, find the point in y \in U that such that s(y) minimises |s(y) - x|
-            ps = [si.LinearNDInterpolator(refpoints, params) for params in sssurfparams.transpose()]
-            self.f = lambda x : surface(np.hstack([p(x)for p in ps]))
+            subparams = np.array(map(nearestpoint, subpoints, [surface]*len(subpoints))) # For each point, x \in F, find the point in y \in U that such that S(y) minimises |S(y) - x|
+            # Now construct the mapping, self.f, which maps from points on the reference sub-simplex to M
+            if subdim==1:
+                p = si.interp1d(refpoints.ravel(), subparams)
+                self.f = lambda x : surface(p(x))
+            else:
+                ps = [si.LinearNDInterpolator(refpoints, params) for params in subparams.transpose()]
+                self.f = lambda x : surface(np.hstack([p(x)for p in ps]))
         else:
-            y = nearestpoint(vertices[subsimplexids])
+            y = nearestpoint(vertices[subidx], surface)
             self.f = lambda x : y
         
-        self.ssidx = subsimplexids
-        self.nonssidx = np.setdiff1d(np.arange(self.dim+1), self.ssverts, True)
-        self.nonssvertices = vertices[self.nonssidx]
+        self.subidx = subidx
+        self.otheridx = np.setdiff1d(np.arange(self.dim+1), self.subidx, True) # the indexes of the non-mapped vertices
+        self.othervertices = vertices[self.otheridx]
         
     def apply(self, x):
-        barycoords = np.dot(x, self.barycentric)
-        ssbarycoords = barycoords[:,self.ssidx[:-1]]
-        curvemappoints = self.f(ssbarycoords)
-        nonssbarycoords = barycoords[:,self.nonssidx]
-        noncurvemappoints = np.dot(nonssbarycoords, self.nonssvertices)
-        return curvemappoints + noncurvemappoints
+        barycoords = np.dot(appendcolumn(x), self.barycentric) # convert x into barycentric coordinates
+        barycoords[barycoords < 0] = 0
+        barycoords[barycoords > 1] = 1 # otherwise the interpolator can get upset - rounding error comes from pinv
+        ssbarycoords = barycoords[:,self.subidx] # extract the barycentric coordinates associated with the mapped sub-simplex
+        curvemappoints = self.f(ssbarycoords[:,:-1]) # map them (we use the first m-1 barycentric coordinates, because those were the reference points)
+        noncurvemappoints = np.dot(barycoords[:,self.otheridx], self.othervertices) # now map the barycentric coordinates not associated with the sub-simplex
+        
+        return curvemappoints*np.sum(ssbarycoords,axis=1).reshape(-1,1) + noncurvemappoints # And add everything up
         
         
          
--- pypwdg/utils/tests/testcurved.py
+++ pypwdg/utils/tests/testcurved.py
@@ -11,14 +11,63 @@ class Test(unittest.TestCase):
 
 
     def testCurve1(self):
+        ''' Map the face x=0 to a parabola '''
         n = 2
         vertices = np.vstack((np.eye(n), np.zeros((1,n))))
-        subsimplexids = np.array([0,1]) 
-        surface = lambda t: np.hstack((t*(1-t)/4, t))
-        fm = puc.FaceMapping(vertices, subsimplexids, surface)
+        subsimplexids = np.array([1,2]) 
+        surface = lambda t: np.hstack((-t*(1-t)/4, t))
+        fm = puc.CurvedMapping(vertices, subsimplexids, surface)
         np.testing.assert_almost_equal(vertices, fm.apply(vertices))
+        N = 10
+        yaxis = np.zeros((10,2))
+        yaxis[:,1] = np.linspace(0,1,N)
+        mapyaxis = fm.apply(yaxis)
+        print mapyaxis
+        np.testing.assert_almost_equal(-mapyaxis[:,1]*(1-mapyaxis[:,1])/4, mapyaxis[:,0])
         
+    def testCurve2(self):
+        ''' Map the face x=0 to a parabola that does not intersect the original face at its vertices '''
+        n = 2
+        vertices = np.vstack((np.eye(n), np.zeros((1,n))))
+        subsimplexids = np.array([1,2]) 
+        surface = lambda t: np.hstack((-t*(1-t)/4-1, t))
+        fm = puc.CurvedMapping(vertices, subsimplexids, surface)
+        N = 10
+        yaxis = np.zeros((10,2))
+        yaxis[:,1] = np.linspace(0,1,N)
+        mapyaxis = fm.apply(yaxis)
+        print mapyaxis
+        np.testing.assert_almost_equal(-mapyaxis[:,1]*(1-mapyaxis[:,1])/4-1, mapyaxis[:,0])
 
+    def testCurve3(self):
+        ''' This time we're just mapping an edge to the parabola '''
+        vertices = np.array([[0,0],[0,1]])
+        subsimplexids = np.array([0,1])
+        surface = lambda t: np.hstack((-t*(1-t)/4-1, t))
+        fm = puc.CurvedMapping(vertices, subsimplexids, surface)
+        
+        N = 10
+        yaxis = np.zeros((10,2))
+        yaxis[:,1] = np.linspace(0,1,N)
+        mapyaxis = fm.apply(yaxis)
+        print mapyaxis
+        np.testing.assert_almost_equal(-mapyaxis[:,1]*(1-mapyaxis[:,1])/4-1, mapyaxis[:,0])
+        
+        
+    def testVertexMap(self):
+        ''' Test that just mapping one vertex works '''
+        n = 2
+        vertices = np.vstack((np.eye(n), np.zeros((1,n))))
+        subsimplexids = np.array([1])
+        surface = lambda t: np.hstack(((1-t)**2+1,t)) # a parabola with its apex at 1,1, which is the closet point to (0,1)
+        fm = puc.CurvedMapping(vertices, subsimplexids, surface)
+        N = 5
+        points = puc.uniformreferencepoints(2, N)
+        mpoints = fm.apply(points)
+        print points
+        print mpoints
+        M = np.array([[1,0],[1,1]]) # the desired map should be a shear
+        np.testing.assert_almost_equal(mpoints, np.dot(points, M))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testCurve1']
