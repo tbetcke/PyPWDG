a8f501537522cffb25a226d197ed59f272e064a0
Joel Phillips <joel.m.phillips@gmail.com>
2011-10-31 22:06:44 +0000

    ...

--- experiments/parallel/mpiexample.py
+++ experiments/parallel/mpiexample.py
@@ -3,16 +3,20 @@ Created on Oct 31, 2011
 
 @author: joel
 '''
+import numpy as np
+
+msgs = [np.ones(10) * i for i in \
+        range(5)]
 
 import mpi4py.MPI as mpi
-import numpy as np
 import time
 comm = mpi.COMM_WORLD
 
 time.sleep(1.0)
 
 if comm.rank==0:
-    msgs = [np.ones(10) * i for i in range(comm.size)]
+    msgs = [np.ones(10) * i for i in 
+            range(comm.size)]
     comm.scatter(msgs)
 else:
     msg = comm.scatter()
