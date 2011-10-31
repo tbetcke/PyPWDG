4f145e45b6d3f51abd49aa67cdaa9a2249b4d05e
Joel Phillips <joel.m.phillips@gmail.com>
2011-10-31 21:26:18 +0000

    mpi example

---
+++ experiments/parallel/mpiexample.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct 31, 2011
+
+@author: joel
+'''
+
+import mpi4py.MPI as mpi
+import numpy as np
+import time
+comm = mpi.COMM_WORLD
+
+time.sleep(1.0)
+
+if comm.rank==0:
+    msgs = [np.ones(10) * i for i in range(comm.size)]
+    comm.scatter(msgs)
+else:
+    msg = comm.scatter()
+    print "My rank is %s"%(comm.rank), msg
+    
