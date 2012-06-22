45b75c984015b14c6b3f70c3019736f4a1d875b7
Joel Phillips <joel.m.phillips@gmail.com>
2012-06-22 18:18:00 +0100

    ...

--- experiments/domaindecomp/comparison.py
+++ experiments/domaindecomp/comparison.py
@@ -37,7 +37,7 @@ def compare(problem, basisrule, mortardegree, nquad, system, plotdata = None):
         bounds, npoints = plotdata
     it = psi.ItTracker()
     solver = psi.GMRESSolver('ctor', it)
-    sm = solveMortar(problem, basisrule, 2, nquad, system, solver)
+    sm = solveMortar(problem, basisrule, 0, nquad, system, solver)
     if plotdata: pom.output2dsoln(bounds, sm, npoints, show=False)
     itsm = np.array(it.reset())
     
@@ -47,9 +47,9 @@ def compare(problem, basisrule, mortardegree, nquad, system, plotdata = None):
     sdd = computation.solution(psd.DomainDecompOperator(problem.mesh), solver)
     if plotdata: pom.output2dsoln(bounds, sdd, npoints, show=False)
     itsdd = np.array(it.reset())
-    sb = computation.solution(psi.BlockPrecondOperator(problem.mesh), solver)
-    if plotdata: pom.output2dsoln(bounds, sb, npoints, show=False)
-    itsb = np.array(it.reset())
+#    sb = computation.solution(psi.BlockPrecondOperator(problem.mesh), solver)
+#    if plotdata: pom.output2dsoln(bounds, sb, npoints, show=False)
+#    itsb = np.array(it.reset())
     
     mp.figure()
     mp.hold(True)
@@ -57,7 +57,7 @@ def compare(problem, basisrule, mortardegree, nquad, system, plotdata = None):
 #    mp.figure()
     mp.semilogy(itsdd, 'r')
 #    mp.figure()
-    mp.semilogy(itsb, 'g')
+#    mp.semilogy(itsb, 'g')
     mp.show()
 #    print itsm, itsdd, itsb
 
